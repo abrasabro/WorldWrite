bd5d8a4094f900fd6971a51279ea1adafb00a689
abrasabro <abrasabro@hotmail.com>
2018-06-04 16:38:44 -0500

    initial commit

---
+++ app/src/main/java/abrasabro/worldwrite/MainActivity.kt
@@ -0,0 +1,19 @@
+package abrasabro.worldwrite
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
